55cd23a36606cfbd07d3c480d8430096be357bce
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-01 14:34:51 +0530

    gateway-routes-configured

--- src/main/java/com/debrup/microservicesapigateway/MicroservicesApiGatewayApplication.java
+++ src/main/java/com/debrup/microservicesapigateway/MicroservicesApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.debrup.microservicesapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicesApiGatewayApplication {
 
     public static void main(String[] args) {
